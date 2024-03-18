39c89bbe9c041c1b1f9f6edf3dee819bbf44f205
apchavan <ameyc96@gmail.com>
2024-03-18 10:42:34 +0530

    Updated comment

--- Shell Scripts/kill_python_script.sh
+++ Shell Scripts/kill_python_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Kill existing process of Dash.
+# Kill existing process of Python.
 script_name=/FULL/PATH/TO/PYTHON_SCRIPT.PY
 ps -ef | grep $script_name | grep -v grep | awk '{print $2}' | xargs kill -9
